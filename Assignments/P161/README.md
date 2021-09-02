0573b6ce02becc626de2c96a3787cff2f7ee2a49
Joshua Beaty <joshua.a.beaty@gmail.com>
2021-09-02 10:16:04 -0500

    got p161 mostly working

---
+++ Assignments/P161/README.md
@@ -0,0 +1,36 @@
+## P161 - Traffic Lights
+### Joshua Beaty
+### Description:
+
+On a given street there are 2 to 100 traffic lights with green light cycles between 10 and 90 seconds in length. The last five seconds of a cycle is orange. 
+
+Write a program that determines how long it will be before all the lights are green again. The timer begins as soon as all the lights show green. Output will be in the **hh:mm:ss** format unless the time takes longer than 5 hours in which case the output will be *"Signals fail to synchronise in 5 hours"*.
+
+#### Example
+
+- Input: 
+    - 19 20   0
+    - 30
+    -   25    35 0
+    - 0 0 0
+- Output: 
+    - 00:00:40
+    - 00:05:00
+
+### Files
+
+|   #   | File                       | Description                                                |
+| :---: | -------------------------- | ---------------------------------------------------------- |
+|   1   | [main.cpp](./main.cpp)     | solution file.                                             |
+|   2   | [input](./input)           | Test input file from problem statement                     |
+|   3   | [input2](./input2)         | Test input file from uDebug user UVA Judge                 |
+|   4   | [input3](./input3)         | Two incorrect cases from input2, working on them           |
+
+
+### Instructions
+
+- .
+
+### Sources
+
+- No sources used so far.
---
+++ Assignments/P161/main.cpp
@@ -0,0 +1,92 @@
+/*
+* Joshua Beaty
+* 4883
+* 09/02/2021
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+  //create integer to hold incoming cycle data
+  int inData = 0;
+  cin >> inData;
+  
+  //loop as long as incoming cycle data is greater than 0
+  while(inData){
+    //create array of light cycles 'lightCycles' of size 110, 110 is enough to hold all cycle data
+    int lightCycles[110];
+    int count = 0;
+    int longestCycle = 0;
+    //loop as long as incoming cycle data is greater than 0
+    while(inData){
+      //put cycle data in array 'lightCycles'
+      lightCycles[count] = inData;
+      //store largest cycle data
+      if(inData > longestCycle)
+        longestCycle = inData;
+      //grab next cycle data
+      cin >> inData;
+      //increase count for array filling purposes
+      count++;
+    }
+    //create an int for current time in seconds
+    //don't need to start at 0 since we need to wait until the longest light turns green again
+    int currentSecond = (longestCycle*2);
+    //create a boolean for synchrony
+    bool inSync = false;
+    //while time is less than 18000 seconds or five hours
+    while(currentSecond <= 18000 && !inSync){
+      //boolean for synchrony, will turn false if orange/red light is found
+      bool greenLights = true;
+      //loop through light cycles for the current time
+      for(int i = 0; i < count; i++){
+        //figure out how many times to increment current light cycle based on current time
+        int increment = currentSecond / lightCycles[i];
+        //if increment is even then it's possible for it to be green, else break
+        if(increment % 2){
+          greenLights = false;
+          break;
+        }
+        //figure out current cycle = incrementation + green light time
+        int currentCycle = (lightCycles[i]*increment) + (lightCycles[i]-5);
+        //find difference in time between now and current cycle, this is the remaining green time
+        int timeDiff = currentCycle - currentSecond;
+        //if timeDiff is greater than 0, we have a green light, check the next lightCycles[i]
+        //else, break current time
+        if(timeDiff <= 0){
+          greenLights = false;
+          break;
+        }
+      }
+      //increase current time
+      currentSecond++;
+      //flip inSync if lights are all green
+      if(greenLights)
+        inSync = true;
+    }
+    //if inSync, print out timeframe, else print 5 hour failure message
+    if(inSync){
+      currentSecond--;
+      int hr = currentSecond / 3600;
+      currentSecond = currentSecond % 3600;
+      int min = currentSecond / 60;
+      currentSecond = currentSecond % 60;
+      cout << setfill('0');
+      cout << setw(2) << hr << ':';
+      cout << setw(2) << min << ':';
+      cout << setw(2) << currentSecond << '\n';
+    }
+    else{
+      cout << "Signals fail to synchronise in 5 hours\n";
+    }
+    //current inData should be 0, if another is found, terminate program
+    cin >> inData;
+    if(!inData){
+      //cout << "Multiple consecutive zeros found, terminating.\n\n";
+      return 0;
+    }
+  }
+}
