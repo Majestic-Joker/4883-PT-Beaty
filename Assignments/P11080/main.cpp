6311e080b5ce97c80f65cca384fa057da053e3cc
Joshua Beaty <joshua.a.beaty@gmail.com>
2021-09-14 17:47:11 -0500

    added debug mode to figure out what's not working

--- Assignments/P11080/main.cpp
+++ Assignments/P11080/main.cpp
@@ -8,12 +8,13 @@
 
 using namespace std;
 
-bool debugMode = false;
+bool debugMode = true;
 
 //created a struct to keep track of some basic information.
 struct Node{
     int totalNeighbors;
     vector<int> neighbors;
+    vector<int> streets;
     bool hasGuard;
     bool guarded;
 
@@ -40,17 +41,19 @@ int main() {
         int totalJunctions = 0;                                      //1<=V<=200
         int totalStreets = 0;                                        //0<=E<=10000                   
         if(debugMode){
-        cout << "totalCases: " << totalCases << '\n';    
-        cout << "minGuards: " << minGuards << '\n';                                                                   
-        cout << "totalJunctions: " << totalJunctions << '\n';
-        cout << "totalStreets: " << totalJunctions << '\n';
+            cout << "Current Case: " << totalCases-i << '\n';     
+            cout << "minGuards: " << minGuards << '\n';                                                                   
+            cout << "totalJunctions: " << totalJunctions << '\n';
+            cout << "totalStreets: " << totalStreets << '\n';
         }
         cin >> totalJunctions;
         cin >> totalStreets;
-        if(debugMode)
-            cout << totalCases << '\n';
-        if(debugMode)
-            cout << totalCases << '\n';
+
+        if(debugMode){                                                                     
+            cout << "totalJunctions: " << totalJunctions << '\n';
+            cout << "totalStreets: " << totalStreets << '\n';
+        }
+
         //if no streets, minimum number of guards would be equal to the number of junctions.
         if(totalStreets == 0){
             minGuards = totalJunctions;
@@ -65,12 +68,18 @@ int main() {
                 junctions.push_back(temp);
                 count--;
             }
+            if(debugMode){   
+                cout << "Number of Nodes: " << junctions.size() << '\n';                                                                   
+            }
             //f and t correspond to the left and right numbers of the street endpoints.
             int f = 0;
             int t = 0;
 
             //loop through to read in street endpoints and set the Node's neighbors
             for(int j = totalStreets; j > 0; j--){
+                if(debugMode && j == totalStreets)
+                    cout << "\nList of Streets\n";
+                
                 cin >> f;
                 cin >> t;
 
@@ -80,17 +89,33 @@ int main() {
                 //increment total neighbors of the node, any node with totalNeighbors = 0 is disjointed and adds to minGuards
                 junctions[f]->totalNeighbors++;
                 junctions[t]->totalNeighbors++;
-                //figure out how to track junctions and streets beyond disjunctions
+                
+                if(debugMode)
+                    cout << "Junction " << f << " is connected to " << t << "\n";
             }
 
             //for every connected node in junctions, if not guarded, place guard and set neighbors to guarded.
-            for(Node* n : junctions){
-                if(n->totalNeighbors > 0){
-                    if(!n->hasGuard && !n->guarded){
-                        n->hasGuard = true;
-                        n->guarded = true;
-                        for(int node : n->neighbors)
+            for(int z = 0; z < junctions.size(); z++){
+                if(debugMode)   
+                    cout << "\nCurrent Node Position: " << z << '\n';                                                                   
+            
+
+                if(junctions[z]->totalNeighbors > 0){
+                    if(debugMode)
+                        cout << "Node " << z << " has " << junctions[z]->totalNeighbors << " neighbors\n";
+                    
+                    if(!junctions[z]->hasGuard && !junctions[z]->guarded){
+                        if(debugMode)
+                            cout << "Node " << z << " has no guards and isn't guarded" << '\n';
+                        junctions[z]->hasGuard = true;
+                        junctions[z]->guarded = true;
+                        if(debugMode)
+                            cout << "Node " << z << " gets a guard and becomes guarded" << '\n';
+                        for(int node : junctions[z]->neighbors){
                             junctions[node]->guarded = true;
+                            if(debugMode)
+                                cout << "Neighbor Node " << node << " becomes guarded.\n";
+                        }
                     }
                 }
             }
