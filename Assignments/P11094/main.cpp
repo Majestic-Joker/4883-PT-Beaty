9c9fe620ce0ed3100ca2f2e0cc0f14e28dd0ad70
Joshua Beaty <joshua.a.beaty@gmail.com>
2021-09-21 03:13:57 -0500

    initial commit for p10194

---
+++ Assignments/P11094/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<string>
+
+using namespace std;
+
+bool debugMode = false;
+
+int main(){
+    //initialize some stuff
+    ofstream fout, debug;
+    fout.open("test.out");
+    
+
+    fout.close();
+}
