e32e055396a4c3a36b93688508a7c9cdc0a3799d
Joshua Beaty <68184850+Majestic-Joker@users.noreply.github.com>
2021-10-01 09:49:32 -0500

    setting up program to receive input from user

--- Assignments/P12732/main.cpp
+++ Assignments/P12732/main.cpp
@@ -6,6 +6,8 @@
 
 #include<iostream>
 #include<fstream>
+#include<string>
+#include<vector>
 
 using namespace std;
 
@@ -15,13 +17,67 @@ bool debugMode = false;
 //when set to true, queries user for test cases instead of server
 bool testing = false;
 
+//Sends output to both the console and the file and prints a new line after each
+void outputLine(string output, ofstream& file, ofstream& dbg){
+    cout << output << '\n';
+    file << output << '\n';
+    if(debugMode)
+        dbg << output << '\n';
+}
+
 int main(){
     //initialize file stuff
     ofstream fout, debug;
     fout.open("test.out");
     debug.open("debug.out");
 
+    if(debugMode)
+        debug << "==Program Initialized==\n\n";
+
     int cases = 0;
+    //when testing is true, we need to prompt the user to provide server output.
+    if(testing)
+        outputLine("How many cases do you want to run? (1-100)", fout, debug);
+
+    //read in the number of cases
+    cin >> cases;
+    if(testing)
+        fout << cases << '\n';
+
+    if(debugMode)
+        debug << "Total number of cases is: " << cases << "\n";
+
+    int caseCount = cases;
+    //loop for the number of cases
+    while(caseCount--){
+        //initialize the case
+        int numCoins = 0;
+        int heavyCoin = -99;
+        vector<int> coins;
+
+        if(debugMode)
+            debug << "Starting Case: " << cases-caseCount << "\n";
+
+        //if testing prompt for coins
+        if(testing)
+            outputLine("How many coins are in this case?", fout, debug);
+
+        cin >> numCoins;
+        if(testing)
+            fout << numCoins << '\n';
+        
+        //if testing, prompt user to decide on a heavy coin and 
+        //store the result for later comparison
+        //repeat prompts 
+        if(testing){
+            while(heavyCoin < 0 || heavyCoin > numCoins){
+                outputLine("Which coin is heaviest? (1-number of coins)", fout, debug);
+                cin >> heavyCoin;
+            }
+        }
+
+        
+    }
 
     
     fout.close();
